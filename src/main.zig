c86eaa276b65138b4c4f75c1c4279c2ef6458bfa
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-01-06 20:19:54 +0100

    Day 2, part 1

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,113 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const cmdsize = u64;
+const prog = [1024]cmdsize;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+pub fn main() !void {
+    var program = init_memory(&.{});
+    try load_program(&program);
+    program[1] = 12;
+    program[2] = 2;
+    run_program(&program);
+    try print_result(&program);
+}
+
+fn init_memory(init: []const cmdsize) prog {
+    var program: prog = std.mem.zeroes(prog);
+    std.mem.copyForwards(cmdsize, &program, init);
+    return program;
+}
+
+fn print_result(program: *prog) !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
+    try stdout.print("{d}", .{program[0]});
     try bw.flush(); // don't forget to flush!
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn load_program(program: *prog) !void {
+    const stdin_file = std.io.getStdIn();
+    const stdin = stdin_file.reader();
+    var number: u32 = 0;
+    var index: u64 = 0;
+    while (true) {
+        const byte: u8 = stdin.readByte() catch |err| {
+            if (err == error.EndOfStream) {
+                program.*[index] = number;
+                return;
+            } else {
+                return err;
+            }
+        };
+        switch (byte) {
+            ',' => {
+                program.*[index] = number;
+                index += 1;
+                number = 0;
+            },
+            '0'...'9' => {
+                number = number * 10 + (byte - '0');
+            },
+            '\n' => {},
+            else => {
+                std.debug.print("parser encountered unexpected byte: {d}\n", .{byte});
+                @panic("parser encountered unexpected byte");
+            },
+        }
+    }
+}
+
+fn run_program(program: *prog) void {
+    var index: u64 = 0;
+    while (true) {
+        const opcode = program.*[index];
+        switch (opcode) {
+            1 => {
+                const param1 = program.*[index + 1];
+                const param2 = program.*[index + 2];
+                const param3 = program.*[index + 3];
+                program.*[param3] = program.*[param1] + program.*[param2];
+                index += 4;
+            },
+            2 => {
+                const param1 = program.*[index + 1];
+                const param2 = program.*[index + 2];
+                const param3 = program.*[index + 3];
+                program.*[param3] = program.*[param1] * program.*[param2];
+                index += 4;
+            },
+            99 => {
+                return;
+            },
+            else => {
+                std.debug.print("program encountered unknown opcode: {d}\n", .{opcode});
+                @panic("program encountered unknown opcode");
+            },
+        }
+    }
+}
+
+test "simple program 1" {
+    var program = init_memory(&.{ 1, 0, 0, 0, 99 });
+    run_program(&program);
+    try std.testing.expectEqual(program, init_memory(&.{ 2, 0, 0, 0, 99 }));
+}
+
+test "simple program 2" {
+    var program = init_memory(&.{ 2, 3, 0, 3, 99 });
+    run_program(&program);
+    try std.testing.expectEqual(program, init_memory(&.{ 2, 3, 0, 6, 99 }));
+}
+
+test "simple program 3" {
+    var program = init_memory(&.{ 2, 4, 4, 5, 99, 0 });
+    run_program(&program);
+    try std.testing.expectEqual(program, init_memory(&.{ 2, 4, 4, 5, 99, 9801 }));
+}
+
+test "simple program 4" {
+    var program = init_memory(&.{ 1, 1, 1, 4, 99, 5, 6, 0, 99 });
+    run_program(&program);
+    try std.testing.expectEqual(program, init_memory(&.{ 30, 1, 1, 4, 2, 5, 6, 0, 99 }));
 }
